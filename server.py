cd48c01bd646f27eb94535fe265c7366957ce06d
Chetan There <chetan.there@mavs.uta.edu>
2017-05-25 15:34:22 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2,44 +2,36 @@
 
 import socket
 import thread
-def splitter(connectionSocket,addr) :
-    print("addr",addr)
-    print("connectionSocket",connectionSocket)
-    #sentence = str(sentence).upper()
+
+def splitter(connectionSocket,addr) :    
     sentence = connectionSocket.recv(1024)
     print("Received data ", sentence)
     file1 = str(sentence)
-    print("SENTENCE= ",sentence)
-    #filen = sentence[5:14]
+    print("SENTENCE= ",sentence)   
     file2 = file1.split()[1]
-    filen = file2[1:]
-    print("filen",filen)
-    #myobj = open("test.txt","r")
+    filen = file2[1:]  
+    
     try : 	
     	myobj = open(filen,"r")
     except :
         headert = "\HTTP/1.1 404 NOT FOUND"
         header = str(headert)
         text = header
-    else :
-        
+    else :        
         headert = "\HTTP/1.1 200 OK"
         header = str(headert)
         text2 = myobj.read()
         text = header +text2
-    print("text=",text)
-    #print("csentence",sentence)
+    
     sentence = text
     connectionSocket.send(sentence)
     myobj.close()
     connectionSocket.close()
-    #connectionSocket.close()
-#serverName = '192.168.0.16'
+    
 serverName = 'localhost'
 serverPort = 5555
-#threadlist = []
+
 serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#serverSocket = socket.socket()
 serverSocket.bind((serverName,serverPort))
 serverSocket.listen(5)
 sfamily  = str(socket.AF_INET)
@@ -53,9 +45,8 @@ print("shostname",shostname)
 print("protocol TCP")
 
 print("Server is ready to receive")
-#connectionSocket, addr = serverSocket.accept()
-#print("connection from ", addr)
-     
+
+
 while(1) :
 
     connectionSocket, addr = serverSocket.accept()
