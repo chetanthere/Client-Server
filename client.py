a05d6ea4aaa01e82960d2ea9be060762a8f629ec
Chetan There <chetan.there@mavs.uta.edu>
2017-05-25 15:32:45 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,21 +1,20 @@
 #client program
 import socket
 import time
+
 serverName = 'localhost'
 serverPort = 5555
-#clientSocket = socket(socket.AF_INET,socket.SOCK_STREAM)
+
 clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#clientSocket = socket.socket()
 clientSocket.connect((serverName,serverPort))
 sentence = raw_input('enter file name: ')
-#sentence = 'xyz'
 print("sentence = ",sentence)
 clientSocket.send(sentence)
 starttime = time.time()
 mSentence = clientSocket.recv(1024)
 endtime = time.time()
 rtt = endtime - starttime
-print("rtt=",rtt) 	
+	
 cfamily  = str(socket.AF_INET)
 ctype = str(socket.SOCK_STREAM)
 ctimeout = str(clientSocket.gettimeout())
